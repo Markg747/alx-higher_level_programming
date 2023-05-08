375f2ffa07594cfda36956b016fac08b9d7fdb90
Mark Kamau <markgitaka@gmail.com>
2023-05-08 04:11:16 -0700

    new stc

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f"{str[39: -62:]}" + f"{str[107: -17:]}" + f"{str[:6]}"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
